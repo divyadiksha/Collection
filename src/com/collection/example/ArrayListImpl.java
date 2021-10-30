4da6d8fcea4975f7c0896fd0f158c757054b4801
divyadiksha <36110455+divyadiksha@users.noreply.github.com>
2021-10-30 17:55:04 +0530

    array list example.

--- src/com/collection/example/ArrayListImpl.java
+++ src/com/collection/example/ArrayListImpl.java
@@ -8,7 +8,8 @@ import java.util.List;
  * Array List uses dynamic array internally.
  * It maintains insertion order.
  * It provides random access.
- * It is not synchronized 
+ * It is not synchronized.
+ * Debug to see the internal processing. 
  * 
  * @author Divya Diksha
  *
@@ -19,10 +20,33 @@ public class ArrayListImpl {
 		
 		List<Integer> arrayList = new ArrayList<>();
 		
+		//insert
 		for(int i=1;i<=10;i++) {
 			arrayList.add(i);
 		}
-		
+		//get
 		System.out.println("5th element:"+arrayList.get(4));
+		
+		//removes element at index 4
+		arrayList.remove(4);
+		System.out.println("5th element:"+arrayList.get(4));
+		
+		//returns size of the array list
+		System.out.println("Size of the arrayList:"+arrayList.size());
+		
+		//checks if the element is present
+		System.out.println("Is 10 present in the arrayList: "+arrayList.contains(10));
+		System.out.println(arrayList);
+		
+		arrayList.add(11);
+		arrayList.add(11);
+		System.out.println("Allows duplicate elements:"+arrayList);
+		
+		arrayList.add(null);
+		System.out.println("Allows null entry:"+arrayList);
+		System.out.println("Is null present in the arrayList: "+arrayList.contains(null));
+		
+		//stream arrayList
+		System.out.println("Number of even elements:"+arrayList.stream().filter(e->e!=null && e%2==0).count());
 	}
 }
